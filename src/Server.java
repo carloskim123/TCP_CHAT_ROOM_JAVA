a22a26bcd7872f3486b9f5de4b33c65367c23329
Carlos Kirui <carloskirui154@gmail.com>
2023-11-06 20:16:16 -0500

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,17 @@
+import java.net.ServerSocket;
+
+public class Server implements Runnable{
+    @Override
+    public void run() {
+        ServerSocket server = new ServerSocket()
+    }
+
+    class ConnectionHandler implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+}
