92a1df482bc05f1272144a3381755f8c5424bd95
Carlos Kirui <carloskirui154@gmail.com>
2023-11-06 20:44:37 -0500

    server actions initialized

--- src/Server.java
+++ src/Server.java
@@ -1,16 +1,131 @@
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server implements Runnable{
-    @Override
-    public void run() {
-        ServerSocket server = new ServerSocket()
+
+    private ArrayList<ConnectionHandler> connections;
+    private ServerSocket server;
+    private boolean done;
+
+    public Server() {
+        connections = new ArrayList<>();
+        done = false;
     }
 
+    @Override
+    public void run() {
+        try {
+            server = new ServerSocket(9999);
+
+            while(!done) {
+                Socket client = server.accept();
+
+                ConnectionHandler handler = new ConnectionHandler(client);
+                connections.add(handler);
+            }
+
+        } catch (IOException e) {
+            // Todo: handle
+            shutdown();
+        }
+    }
+
+    public void broadcast(String message) {
+        for (ConnectionHandler ch : connections) {
+            if(ch != null) {
+                ch.sendMessage(message);
+            }
+        }
+    }
+
+
+    public void shutdown() {
+        try {
+            done = true;
+            if (!server.isClosed()) {
+                server.close();
+            }
+
+            for (ConnectionHandler ch: connections) {
+                ch.shutdown();
+            }
+        } catch (Exception e) {
+            // ignore
+        }
+    }
     class ConnectionHandler implements Runnable {
+
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        private String nickname;
+
+
+
+        public ConnectionHandler (Socket client) {
+            this.client = client;
+        }
 
         @Override
         public void run() {
+            try {
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("Please enter a nickname: ");
+                nickname = in.readLine();
+                System.out.println(nickname + " connected!");
+                broadcast(nickname + " joined the chat!");
 
+                String message;
+                while((message = in.readLine()) != null) {
+                    if(message.startsWith("/nick ")) {
+                        // Todo: Handle nickname change
+                        String[] messageSplit = message.split(" ", 2);
+                        if(messageSplit.length == 2) {
+                            broadcast(nickname + " renamed themselves to " + messageSplit[1]);
+                            System.out.println(nickname + " renamed themselves to " + messageSplit[1]);
+
+                            nickname = messageSplit[1];
+                            out.println("Successfully changed nickname to " + nickname);
+                        } else {
+                            out.println("No nickname provided");
+                        }
+                    } else if(message.startsWith("/quit ")) {
+                        broadcast(nickname + " left the chat!");
+                         // Todo: handle quit
+                    } else {
+                        broadcast(nickname + ": " + message);
+                    }
+                }
+            }
+            catch (Exception e) {
+                try {
+                    shutdown();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        }
+
+
+        public void sendMessage(String message) {
+            System.out.println(message);
+        }
+
+        public void shutdown() throws IOException {
+            try {
+
+                in.close();
+                out.close();
+                if (!client.isClosed()) {
+                    client.close();
+                }
+            } catch (IOException e) {
+                // Todo: handle
+            }
         }
     }
 
